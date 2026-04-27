98b18139f3f5cec1005076cc23e13f807c712023
AforArdi <binsbro36@gmail.com>
2026-04-27 23:42:32 +0600

    google login implemented

--- src/components/homepage/RightSideBar.jsx
+++ src/components/homepage/RightSideBar.jsx
@@ -1,11 +1,21 @@
+'use client'
+
+import { authClient } from "@/lib/auth-client";
+import { createAuthClient } from "better-auth/client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const RightSideBAr = () => {
+    const handleGoogleSignIn = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+    }
+
     return (
         <div>
             <h2 className="text-2xl font-semibold text-[#403F3F]">Login with</h2>
             <div className="mt-4 flex flex-col justify-center gap-4">
-                <button className="btn btn-outline text-blue-500"><FaGoogle></FaGoogle> Login with Google</button>
+                <button onClick={handleGoogleSignIn} className="btn btn-outline text-blue-500"><FaGoogle></FaGoogle> Login with Google</button>
                 <button className="btn btn-outline"><FaGithub></FaGithub> Login with Github</button>
             </div>
         </div>
