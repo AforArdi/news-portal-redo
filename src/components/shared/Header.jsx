29e69123a8742b8063513d70ddf24ccbe736201c
AforArdi <binsbro36@gmail.com>
2026-04-27 14:42:28 +0600

    Header designed

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,6 +1,14 @@
+import logo from '@/assets/logo.png';
+import Image from 'next/image';
+import { format } from "date-fns";
+
 const Header = () => {
     return ( 
-        <div></div>
+        <div className='container mx-auto mt-10 flex flex-col justify-center items-center gap-3'>
+            <Image src={logo} alt='Dragon News Logo' width={400} height={60}></Image>
+            <p className='text-[18px] text-[#706F6F]'>Journalism Without Fear or Favour</p>
+            <p className='text-xl font-medium text-[#706F6F]'>{format(new Date(2014, 1, 11), "yyyy-MM-dd")}</p>
+        </div>
      );
 }
  
